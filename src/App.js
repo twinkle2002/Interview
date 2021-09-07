c0c927552d81f1fbd29f45b4eb4ab83120a2acef
Twinkle <twinktwinkle20@gmail.com>
2021-09-07 11:08:34 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import List from './componenets/List';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1 className="my-4 text-center">List Of Candidates for The Interview</h1>
+
+      <List/>
+    </>
   );
 }
 
---
+++ src/componenets/List.js
@@ -0,0 +1,186 @@
+import React from "react";
+
+
+
+
+function List() {
+  return (
+    <div>
+      <p>
+        <button
+          className="btn btn-primary mx-2 my-1 "
+          data-bs-toggle="collapse"
+          href="#multiCollapseExample1"
+          role="button"
+          aria-expanded="false"
+          aria-controls="multiCollapseExample1"
+          
+        >
+          Scheduled Candidate
+        </button>
+        <button
+          className="btn btn-primary mx-2 my-1"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#multiCollapseExample2"
+          aria-expanded="false"
+          aria-controls="multiCollapseExample2"
+        >
+          Unscheduled Candidate
+        </button>
+        {/* <button className="btn btn-primary gap-2" type="button" data-bs-toggle="collapse" data-bs-target=".multi-collapse" aria-expanded="false" aria-controls="multiCollapseExample1 multiCollapseExample2">Toggle both elements</button> */}
+      </p>
+      <div className="row">
+        <div className="col">
+          <div className="collapse multi-collapse" id="multiCollapseExample1">
+          <i class="bi bi-person"></i>
+            <li className="list-group list-group-numbered">
+            
+              <li className="list-group-item d-flex justify-content-between align-items-start">
+                <div className="ms-2 me-auto">
+                  <div className="fw-bold"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person" viewBox="0 0 16 16">
+      <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z"/>
+    </svg> Ashish Verma</div>
+                  
+                  <ul>
+                    <li>M.E. Artificial Inteligence</li>
+                    <li>Year Of Experience: 4</li>
+                    <li>Interview Date: 8th September</li>
+                  </ul>
+
+                </div>
+                <span className="badge bg-success rounded-pill">Shedule Date-Time</span>
+              </li>
+              <li className="list-group-item d-flex justify-content-between align-items-start">
+                <div className="ms-2 me-auto">
+                  <div className="fw-bold"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person" viewBox="0 0 16 16">
+      <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z"/>
+    </svg> Kartik Shah</div>
+                  <ul>
+                    <li>M.E. Machine Learning</li>
+                    <li>Year Of Experience: 3</li>
+                    <li>Interview Date: 8th September</li>
+                  </ul>
+                </div>
+                <span className="badge bg-success rounded-pill">Schedule Date-Time</span>
+              </li>
+              <li className="list-group-item d-flex justify-content-between align-items-start">
+                <div className="ms-2 me-auto">
+                  <div className="fw-bold"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person" viewBox="0 0 16 16">
+      <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z"/>
+    </svg> Kirti Patel</div>
+                  <ul>
+                    <li>M.E. Computer Science</li>
+                    <li>Year Of Experience: 2</li>
+                    <li>Interview Date: 9th September</li>
+                  </ul>
+                </div>
+                <span className="badge bg-success rounded-pill">Schedule Date-Time</span>
+              </li>
+              <li className="list-group-item d-flex justify-content-between align-items-start">
+                <div className="ms-2 me-auto">
+                  <div className="fw-bold"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person" viewBox="0 0 16 16">
+      <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z"/>
+    </svg> Zeel Patel</div>
+                  <ul>
+                    <li>M.E. Data Structur</li>
+                    <li>Year Of Experience: 5</li>
+                    <li>Interview Date: 9th September</li>
+                  </ul>
+                </div>
+                <span className="badge bg-success rounded-pill">Schedule Date-Time</span>
+              </li>
+              <li className="list-group-item d-flex justify-content-between align-items-start">
+                <div className="ms-2 me-auto">
+                  <div className="fw-bold"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person" viewBox="0 0 16 16">
+      <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z"/>
+    </svg> Subh Dixit</div>
+                  <ul>
+                    <li>M.E. B.sc</li>
+                    <li>Year Of Experience: 4</li>
+                    <li>Interview Date: 8th September</li>
+                  </ul>
+                </div>
+                <span className="badge bg-success rounded-pill">Schedule Date-Time</span>
+              </li>
+            </li>
+          </div>
+        </div>
+        <div className="col">
+          <div className="collapse multi-collapse" id="multiCollapseExample2">
+            <li className="list-group list-group-numbered">
+              <li className="list-group-item d-flex justify-content-between align-items-start">
+                <div className="ms-2 me-auto">
+                  <div className="fw-bold"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person" viewBox="0 0 16 16">
+      <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z"/>
+    </svg> Krina Shah</div>
+                  <ul>
+                    <li>M.E. Automobile</li>
+                    <li>Year Of Experience: 2</li>
+                    <li>Interview Date: Not Set</li>
+                  </ul>
+                </div>
+                <span className="badge bg-success rounded-pill">Schedule Date-Time</span>
+              </li>
+              <li className="list-group-item d-flex justify-content-between align-items-start">
+                <div className="ms-2 me-auto">
+                  <div className="fw-bold"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person" viewBox="0 0 16 16">
+      <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z"/>
+    </svg> Dhwani Raj</div>
+                  <ul>
+                    <li>B.E. Information Technology</li>
+                    <li>Year Of Experience: 1</li>
+                    <li>Interview Date: Not Set</li>
+                  </ul>
+                </div>
+                <span className="badge bg-success rounded-pill">Schedule Date-Time</span>
+              </li>
+              <li className="list-group-item d-flex justify-content-between align-items-start">
+                <div className="ms-2 me-auto">
+                  <div className="fw-bold"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person" viewBox="0 0 16 16">
+      <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z"/>
+    </svg> Tej Sharma</div>
+                  <ul>
+                    <li>M.E. Machine Learning</li>
+                    <li>Year Of Experience: 2</li>
+                    <li>Interview Date: Not Set</li>
+                  </ul>
+                </div>
+                <span className="badge bg-success rounded-pill">Schedule Date-Time</span>
+              </li>
+              <li className="list-group-item d-flex justify-content-between align-items-start">
+                <div className="ms-2 me-auto">
+                  <div className="fw-bold"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person" viewBox="0 0 16 16">
+      <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z"/>
+    </svg> Vraj Soni</div>
+                  <ul>
+                    <li>M.E.Artificial Inteligence</li>
+                    <li>Year Of Experience: 3</li>
+                    <li>Interview Date: Not Set</li>
+                  </ul>
+                </div>
+                <span className="badge bg-success rounded-pill">Schedule Date-Time</span>
+              </li>
+              <li className="list-group-item d-flex justify-content-between align-items-start">
+                <div className="ms-2 me-auto">
+                  <div className="fw-bold"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person" viewBox="0 0 16 16">
+      <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z"/>
+    </svg> Dhurv Patel</div>
+                  <ul>
+                    <li>M.E. Computer Science</li>
+                    <li>Year Of Experience: 2</li>
+                    <li>Interview Date: Not Set</li>
+                  </ul>
+                </div>
+                <span className="badge bg-success rounded-pill">Schedule Date-Time</span>
+              </li>
+            </li>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default List;
+
